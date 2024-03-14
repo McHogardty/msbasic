a0ab2817360c687e2074e5948074bd243b761045
McHogardty <9075296+McHogardty@users.noreply.github.com>
2024-03-14 23:14:52 +1100

    Add minimal changes needed to get it running on the breadboard 6502.

---
+++ bb6502/bios.s
@@ -0,0 +1,49 @@
+
+.setcpu "65C02"
+.debuginfo
+.segment "BIOS"
+.include "65c51.inc"
+
+LOAD:
+    rts
+
+SAVE:
+    rts
+
+
+ISCNTC:
+    rts
+
+MONRDKEY:
+CHRIN:
+    lda ACIA::STATUS
+    and #(RDRF)
+    beq @nokeypress
+    lda ACIA::DATA
+    jsr CHROUT
+    sec
+    rts
+@nokeypress:
+    clc
+    rts
+
+MONCOUT:
+CHROUT:
+    pha
+    sta ACIA::DATA
+    lda #$ff
+@delay:
+    dec
+    bne @delay
+    pla
+    rts
+
+.include "wozmon.s"
+
+; Interrupt Vectors
+
+    .segment "RESETVECTORS"
+
+    .WORD $0F00     ; NMI
+    .WORD RESET     ; RESET
+    .WORD $0000     ; BRK/IRQ
